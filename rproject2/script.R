8a9a13907695050db6f042573905d90b9bf7188d
rareba <vannini.gv@gmail.com>
2017-06-13 13:42:16 +0200

    rev2

--- rproject2/script.R
+++ rproject2/script.R
@@ -36,20 +36,21 @@ Y ~ poisson(Lambda);
 }
 '
 
-# 2) Ottieni un campione simulato dalla a-posteriori
-
 require(rstan)
 iniTime <- date()
 mod <- stan_model(model_code = modPG)
-endTime <- date();c(iniTime = iniTime, endTime = endTime)
+endTime <- date();
+c(iniTime = iniTime, endTime = endTime)
 
-#SAMPLING E GENERAZIONE DELLE CATENE 
+# 2) Ottieni un campione simulato dalla a-posteriori
+
+#SAMPLING E GENERAZIONE DELLE CATENE A POSTERIORI
 library(rstudioapi)
 system.time(
   fit <<- sampling(mod,
                   data = data,
                   pars = c("Lambda"), #a noi interessa capire la distribuzione di Lambda, non di Y. 
-#Perchè dopo che si campiona lambda si capisce come distribuisce la Y
+                                      #Perchè dopo che si campiona lambda si capisce come distribuisce la Y
                   chains = 4,
                   iter = 25000,
                   warmup = 10000,
@@ -63,17 +64,14 @@ system.time(
 
 testModel <- stan(model_code = modPG, data = data, iter = 100, chains = 4)
 
-# 3) Esplora le diagnostiche di outlput e discuti i risultati
+# 3) Esplora le diagnostiche di output e discuti i risultati
 
 require(ggmcmc)
 outSim <- ggs(fit)
 print(outSim)
 
-# creiamo un file pdf di tutto l'output
-ggmcmc(outSim)
-
 ###traceplots
-library(ggthemes)
+require(ggthemes)
 ggs_traceplot(outSim) + theme_fivethirtyeight()
 
 ### Densita
@@ -93,17 +91,20 @@ ci(outSim)
 
 # sintesi per coppie di parametri
 ggs_pairs(outSim, lower = list(continuous = "density"))
-str(outSim)
 
 # riassunti numeri e diagnostica
 summary(fit)
 
 # 4) Riassumi le caratteristiche principali della distribuzione a-posteriori
 
-#                Commentare
+#   A vedere dal traceplot e dal density plot la sovrapposizione della 4 catene è buona. Magari dal density plot soprattutto si nota qualche discrepanza in più
+#   Gelman invece mostra che non ci sono fattori di potenziale riduzione di scala per Lambda. Il che avalla la tesi sulla discreta bontà del modello.
+#   Bontà avallata anche da Geweke, dato che tutte le catene sono comprese su valori Z fra -2 e 2
+#   Il grafico Caterpillar mostra invece che la credibilità di lambda è alta fra circa 4,5 e 6,25
+#   gg_pairs non funziona per qualche motivo
+#   La funzione summary() aiuta a monitorare numericamente quanto detto fin'ora: vale a dire che le catene si sovrappongono e non ci sono problemi con il modello.
 
 # 5) Ottieni la distribuzione prededittiva della futura osservazione con un campione di dimensione 10k. 
-#    Riassumi le sue caratteristiche.
 
 modPGp <- '
 data {
@@ -148,11 +149,45 @@ system.time(
 )
 
 require(ggmcmc)
+outSim <- ggs(fit)
+print(outSim)
 
-outSim = ggs(fit)
-MailsOsservate <- c(mean(Y))
-ggs_density(outSim, family = "Y_predict") + geom_vline(xintercept = MailsOsservate, color = "red")
+###traceplots
+require(ggthemes)
+ggs_traceplot(outSim) + theme_fivethirtyeight()
+
+### Densita
+ggs_density(outSim) + theme_solarized(light = TRUE)
+
+### R cappello ovvero diagnostica di Gelman
+ggs_Rhat(outSim) + xlab("R_hat")
+
+### diagnostica di Geweke (solo per normali standardizzate)
+ggs_geweke(outSim)
+
+### diagnostica con Caterpillar
+ggs_caterpillar(outSim)
+
+#intervalli di credibilit?
+ci(outSim)
+
+# sintesi per coppie di parametri
+ggs_pairs(outSim, lower = list(continuous = "density"))
+
+# riassunti numeri e diagnostica
+summary(fit)
+
+#    Riassumi le sue caratteristiche.
+
+
 
 # 6) Confronta i risultati ottenuti via simulazione MCMC con i risultati esatti sopra riportati
 
-#    Commentare
+outSim = ggs(fit)
+MailsOsservate <- c(mean(Y))
+ggs_density(outSim, family = "Y_predict") + geom_vline(xintercept = MailsOsservate, color = "red") + geom_vline(xintercept = Y, color = "blue")
+
+#    Mediamente arrivano 5,6 email al giorno. 
+#    Il valore sta dentro alla distribuzione che risulta dal modello predittivo.
+#    Si può dire che ci si può aspettare fra le 5 e le 6 email al giorno.
+#    Guardando anche i valori osservati si può dire che in generale non ci sono "fughe" verso valori improbabili
